c67f7f5e3e7adc3375a6ba5120f543af0af03cb2
rahmatefe <153784733+rahmatefe@users.noreply.github.com>
2023-12-21 18:28:18 +0800

    Add files via upload

---
+++ 231031068-Rahmat Efendi-tugas1-10c.py
@@ -0,0 +1,9 @@
+#buat program kelipatan 4
+a = int(input("Masukkan bilangan A: "))
+
+if a % 4 == 0:
+    print(f"{a} adalah bilangan Kelipatan 4")
+else:
+    print(f"{a} adalah bilangan Bukan Kelipatan 4")
+
+
